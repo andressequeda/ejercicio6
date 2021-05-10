87e6dc6389c5022ecd6d32a2a21e5f7315b90c11
Andres Sequeda Higuita <80718240+andressequeda@users.noreply.github.com>
2021-05-09 22:01:23 -0500

    Add files via upload

---
+++ ejercicio6.py
@@ -0,0 +1,4 @@
+venta  =  int ( input ( "Ingrese el valor de la venta:" ))
+iva  =  venta  *  0.19
+print ( "El valor de la venta es:" , venta , "El valor del IVA es:" , iva ,
+      "El valor de la venta más IVA es:" , venta  + ( venta  *  0.19 ))
